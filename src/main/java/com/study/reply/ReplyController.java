f0088c5a1090e7fa47a2d90c9775683b6425f405
ksu577 <ksu577@naver.com>
2023-11-06 14:04:03 +0900

    순욱

--- src/main/java/com/study/reply/ReplyController.java
+++ src/main/java/com/study/reply/ReplyController.java
@@ -1,25 +1,26 @@
-package com.study.reply;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
-@RestController
-public class ReplyController {
-
-    @    @RequestMapping(value = "/reply/replyList.wow")
-public Map<String, Objects> replyList()
-
-
-    @ResponseBody
-    @RequestMapping(value = "/reply/replyList.wow")
-    public List<ReplyVO> replyList(){
-        List<ReplyVO> replyList=getReplyList();
-        return replyList;
-
-    }
-}
+//package com.study.reply;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Objects;
+//
+//@RestController
+//public class ReplyController {
+//
+//
+//    @RequestMapping(value = "/reply/replyList.wow")
+//    public Map<String, Objects> replyList()
+//
+//
+//    @ResponseBody
+//    @RequestMapping(value = "/reply/replyList.wow")
+//    public List<ReplyVO> replyList() {
+//        List<ReplyVO> replyList = getReplyList();
+//        return replyList;
+//
+//    }
+//}
