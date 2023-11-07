f092034d4e428ee2fcc0602692fce6b86bf0ae5e
ksu577 <ksu577@naver.com>
2023-11-07 17:46:59 +0900

    순욱

--- src/main/java/com/study/member/service/MemberServiceImpl.java
+++ src/main/java/com/study/member/service/MemberServiceImpl.java
@@ -1,14 +1,44 @@
 package com.study.member.service;
 
+import com.study.exception.BizException;
+import com.study.exception.BizNotFoundException;
 import com.study.member.dao.IMemberDao;
+import com.study.member.vo.MemberVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class MemberServiceImpl {
+public class MemberServiceImpl implements IMemberService{
 
     @Autowired
     IMemberDao memberDao;
+
+    @Override
+    public List<MemberVO> getMemberList() {
+        return null;
+    }
+
+    @Override
+    public MemberVO getMember(String Id) throws BizNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void modifyMember(MemberVO memberVO) throws BizException {
+
+    }
+
+    @Override
+    public void removeMember(MemberVO memberVO) throws BizException {
+
+    }
+
+    @Override
+    public void registMember(MemberVO memberVO) throws BizException {
+
+    }
 
 //    @Override
 //    public List<MemberVO> getMemberList() {
