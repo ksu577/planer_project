514fcf089103b48debd19b1b0176f941d56725b9
ksu577 <ksu577@naver.com>
2023-11-10 12:16:22 +0900

    순욱

--- src/main/java/com/study/cart/vo/CartVO.java
+++ src/main/java/com/study/cart/vo/CartVO.java
@@ -1,8 +1,11 @@
 package com.study.cart.vo;
 
+import lombok.Data;
+import lombok.Getter;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+@Data
 public class CartVO {
 
     private String orderNum;
@@ -12,54 +15,6 @@ public class CartVO {
     private String productImg;
     private int productAmount;
 
-
-    public String getOrderNum() {
-        return orderNum;
-    }
-
-    public void setOrderNum(String orderNum) {
-        this.orderNum = orderNum;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getProductNum() {
-        return productNum;
-    }
-
-    public void setProductNum(int productNum) {
-        this.productNum = productNum;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getProductImg() {
-        return productImg;
-    }
-
-    public void setProductImg(String productImg) {
-        this.productImg = productImg;
-    }
-
-    public int getProductAmount() {
-        return productAmount;
-    }
-
-    public void setProductAmount(int productAmount) {
-        this.productAmount = productAmount;
-    }
 
     @Override
     public String toString() {
