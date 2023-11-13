6c1acd85b3f3ba170e09234df3937c3a775c1970
ksu577 <ksu577@naver.com>
2023-11-13 14:24:20 +0900

    순욱

---
+++ src/main/java/com/study/cart/dao/CartDao.java
@@ -0,0 +1,37 @@
+package com.study.cart.dao;
+
+import com.study.cart.vo.CartVO;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+
+
+
+@Repository
+public interface CartDao {
+//추가하기 위한 추
+
+
+
+    // 1. 장바구니 추가
+    public void insertCart(CartVO cartVO);
+
+    // 2. 장바구니 목록
+    public List<CartVO> CartList(String userId);
+
+    // 3. 장바구니 삭제
+    public void delete(int cartId);
+
+    // 4. 장바구니 수정
+    public void modifyCart(CartVO cartVO);
+
+    // 5. 장바구니 금액 합계
+    public int sumMoney(String userId);
+
+    // 6. 장바구니에 있는 것과 동일한 상품 코드 확인
+    public int CountCart(int prodcut_id, String userId);
+
+    // 7. 장바구니 상품수량 변경
+    public void updateCart(CartVO cartVo);
+}
+
