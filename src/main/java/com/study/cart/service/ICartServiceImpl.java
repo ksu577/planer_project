8f1ab0b2414242cab30cd583a7a0759a505bf943
ksu577 <ksu577@naver.com>
2023-11-13 15:40:03 +0900

    순욱

--- src/main/java/com/study/cart/service/ICartServiceImpl.java
+++ src/main/java/com/study/cart/service/ICartServiceImpl.java
@@ -5,7 +5,6 @@ import com.study.cart.vo.CartVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.inject.Inject;
 import java.util.List;
 
 @Service
