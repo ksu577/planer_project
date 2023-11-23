0992c0c4285e5e198fd225a9119e83a16a9b63a8
cola2bear <rjseka918@naver.com>
2023-11-23 14:46:57 +0900

    성민

--- src/main/java/com/study/common/vo/PagingVO.java
+++ src/main/java/com/study/common/vo/PagingVO.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 public class PagingVO implements Serializable {
     //입력받는 데이터
     private int curPage = 1;           // 현재 페이지 번호
-    private int rowSizePerPage = 10;   // 한 페이지당 레코드 수      기본10
+    private int rowSizePerPage = 15;   // 한 페이지당 레코드 수      기본10
     private int pageSize = 10;         // 페이지 리스트에서 보여줄 페이지 갯수  이거는 보통 10 or 5 안 변함
     private int totalRowCount;      // 총 레코드 건수
 
