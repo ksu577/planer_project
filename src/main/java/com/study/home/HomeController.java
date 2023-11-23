e0a5ebad291f4683c5966ea153fdb221aba5b789
duswns <duswns99@gmail.com>
2023-11-23 15:14:08 +0900

    김연준이용~

--- src/main/java/com/study/home/HomeController.java
+++ src/main/java/com/study/home/HomeController.java
@@ -1,28 +1,37 @@
 package com.study.home;
 
+import com.study.plan.service.IPlanService;
+import com.study.plan.vo.PlanVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
     final private HomeService homeService;
+    final private IPlanService planService;
 
     @Autowired
-    public HomeController(HomeService homeService) {
+    public HomeController(HomeService homeService, IPlanService planService) {
         this.homeService = homeService;
+        this.planService = planService;
     }
 
     @RequestMapping("/")
     public String home(Model model){
+        List<PlanVo> planService = this.planService.topPlan();
+        model.addAttribute("topPlan", planService);
         return "/home";
     }
 
     public static void main(String[] args) {
         System.out.println("a");
     }
+
 
 
 }
