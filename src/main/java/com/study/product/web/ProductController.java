f9e7c4d9208382934ab1f89a5c9b4e05c6d15a91
ksu577 <ksu577@naver.com>
2023-11-29 09:02:18 +0900

    순욱

--- src/main/java/com/study/product/web/ProductController.java
+++ src/main/java/com/study/product/web/ProductController.java
@@ -225,15 +225,6 @@ public class ProductController {
 //        return "/shop/afterpay";
 //    }
 
-//    public File getFileFromAttachVO(ProductVO productVO) throws IOException, BizNotFoundException {
-//        String fileName = productVO.getImg();  //저장되어있는 파일이름. 랜덤값
-//        String filePath = productVO.getImgPath();     // 저장되어있는 폴더 경로
-//        String path = uploadPath + File.separatorChar + filePath;
-//        File file = new File(path, fileName);
-//        if (!file.isFile()) throw new BizNotFoundException("파일없음");
-//        return  file;
-//
-//    }
 
     //img파일 썸네일
     @RequestMapping("/imgDownload/showImg.wow")
